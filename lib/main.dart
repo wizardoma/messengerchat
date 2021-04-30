f74d79011a3528b81d208b616e7583bed99235f8
Ibekason Alexander <alibekason@gmail.com>
2021-04-30 20:01:56 +0100

    Bootstrap main screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:messengerchat/presentation/auth/signin_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Messenger",
-
+      home: SignInScreen(),
     );
   }
 }
