f64ffc81a45ab3f010127880899b9ac2fb2bb468
Ibekason Alexander <alibekason@gmail.com>
2021-04-30 21:06:26 +0100

    Add Firebase initializer

---
+++ lib/application/services/auth.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  getCurrentUser() {
+    return _auth.currentUser;
+  }
+
+
+}
